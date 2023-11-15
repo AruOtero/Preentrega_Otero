ccb3a54d89708972b0f42a7abea854de454d9997
Ariana Otero <arianaotero90@gmail.com>
2023-11-15 17:14:10 -0300

    segunda entrega

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,3 +1,20 @@
+/* /* 
+const task = new Promise((resuelto,rechazado)=>{ //pendiente-cumplida-rechazada(perdida)//res,rej
+  resuelto('toma lo que me prestaste')
+
+})
+ */
+/* export const ItemListContainer = ({ greeting }) => {
+
+  /* task
+  .then(respuestaDelResuelto => console.log(respuestaDelResuelto)) */
+ /*  return (
+    <div>
+      <h2 className="text-center">{ greeting }</h2>
+    </div>
+  )
+} */
+
 
 export const ItemListContainer = ({ greeting }) => {
   return (
@@ -6,4 +23,3 @@ export const ItemListContainer = ({ greeting }) => {
     </div>
   )
 }
-
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -30,6 +30,7 @@ export const NavBar = () => {
           </Navbar.Collapse>
         </Container>
       </Navbar>
+      
     </>
   )
 };
